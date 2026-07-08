61c26bcb684f4d58270446d322012434f80de0b6
shake GoGo <alpina.b8.4.6@gmail.com>
2026-07-08 21:20:10 +0900

    feat: implement single-instance enforcement using a named mutex in App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Threading;
 using PortaFile.Services;
 using PortaFile.ViewModels;
 using PortaFile.Views;
@@ -7,8 +8,17 @@ namespace PortaFile;
 
 public partial class App : Application
 {
+    private const string SingleInstanceMutexName = @"Local\PortaFile.SingleInstance";
+    private Mutex? _singleInstanceMutex;
+
     protected override void OnStartup(StartupEventArgs e)
     {
+        if (!TryAcquireSingleInstanceMutex())
+        {
+            Shutdown();
+            return;
+        }
+
         base.OnStartup(e);
 
         var mainWindow = new MainWindow();
@@ -17,6 +27,42 @@ public partial class App : Application
 
         mainWindow.DataContext = viewModel;
         mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        ReleaseSingleInstanceMutex();
+        base.OnExit(e);
+    }
+
+    private bool TryAcquireSingleInstanceMutex()
+    {
+        _singleInstanceMutex = new Mutex(true, SingleInstanceMutexName, out var createdNew);
+        if (createdNew)
+        {
+            return true;
+        }
+
+        _singleInstanceMutex.Dispose();
+        _singleInstanceMutex = null;
+        return false;
+    }
+
+    private void ReleaseSingleInstanceMutex()
+    {
+        if (_singleInstanceMutex != null)
+        {
+            try
+            {
+                _singleInstanceMutex.ReleaseMutex();
+            }
+            catch
+            {
+                // Ignore
+            }
+            _singleInstanceMutex.Dispose();
+            _singleInstanceMutex = null;
+        }
     }
 }
 
